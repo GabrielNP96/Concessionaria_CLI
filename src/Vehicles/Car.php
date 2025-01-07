dff8e26f7a98feca1f018940f6d4a2fb9e8d4117
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-07 17:49:15 +0000

    Classe Car

--- src/Vehicles/Car.php
+++ src/Vehicles/Car.php
@@ -1,7 +1,15 @@
 <?php
-
+namespace App\Vehicles\Car;
 require_once __DIR__ . '/../../vendor/autoload.php';
 use App\Vehicles\Vehicle;
 
-$car = new Vehicle("Uno","Fiat", 1998, "Cinza");
-var_dump($car);
+class Car extends Vehicle {
+    protected float $engine;
+    protected int $carDoorsQuantity;
+
+    function __construct(string $name, string $manufacturer, int $year, string $color, float $engine, int $carDoorsQuantity) {
+        parent::__construct($name, $manufacturer, $year, $color);
+        $this->engine = $engine;
+        $this->carDoorsQuantity = $carDoorsQuantity;
+    }
+}
